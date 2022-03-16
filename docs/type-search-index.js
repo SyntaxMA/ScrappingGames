812119ab4c7c021eebd402d60a6fb4ecccc13179
juego <maciasalvarezruben@elpuig.xeill.net>
2022-03-16 19:01:25 +0100

    Javadoc 3.0

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"App"},{"p":"<Unnamed>","l":"CSV"},{"p":"<Unnamed>","l":"JAXB"},{"p":"<Unnamed>","l":"Juego"},{"p":"<Unnamed>","l":"Juegos"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Menu"},{"p":"<Unnamed>","l":"SO"}];updateSearchResults();
